2e492e2a6db31fc656345130c2aaefc88cab38da
GIGABYTE\ROLEX24 <krisnanananthaa@gmail.com>
2025-06-03 02:16:05 +0800

    Sistem Pelepasan Tugas Pansyarah ILPS

--- login_proses_admin.php
+++ login_proses_admin.php
@@ -2,23 +2,21 @@
 include "conn.php";
 session_start();
 if(isset($_POST['login'])){
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-//echo $username;
-//echo $password;
-//Checking is user existing in the database or not
-    $query = "SELECT * FROM admin WHERE username='{$username}'and password='{$password}'";
-    $result = mysqli_query($conn,$query);
-//semak@baca coding satu persatu baris
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Checking if user exists in the database
+    $query = "SELECT * FROM admin WHERE username='{$username}' AND password='{$password}'";
+    $result = mysqli_query($conn, $query);
+
     $rows = mysqli_num_rows($result);
-    if($rows==1){
+    if($rows == 1){
         $_SESSION['username'] = $username;
         $_SESSION['password'] = $password;
-        
+
         header("Location: admin.php");
-    }else{
-        echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>Click here to <a href='index.php'>Login</a></div>";
+    } else {
+        echo '<div class="form"><h3>USERNAME/PASSWORD IS INCORRECT.</h3><br/>Click here to <a href="index.php">Login</a></div>';
     }
-}else{
 }
 ?>
